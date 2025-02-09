35d7796efb6bdd1a1f1e557b237483e2c247196d
Nur Hamim <nurhamim@Nurs-MacBook-Air.local>
2025-02-09 23:10:53 +0700

    Fix asset path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://hamimistimewa.github.io/web-resume-nurhamim',
+  base: "./",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
